4543d7652c22e4765e99f0311e47e64e6d94e18e
Grasjusz <46102483+grasjusz@users.noreply.github.com>
2024-12-19 00:05:03 +0100

    01.practice

--- 01.practice.php
+++ 01.practice.php
@@ -1,6 +1,36 @@
 <?php
 
 $input1 = readline("Input your first digit: ");
-$input2 = readline("Input your second digit: ");
+$input2 = readline("Input operator(+, -, *, /, %): ");
+$input3 = readline("Input your second digit: ");
 
-echo $input1 + $input2;
+switch ($input2){
+    case ("/"):
+        switch ($input1 or $input3){
+            case (0):
+                echo "You cannot dive by zero!";
+        }
+
+        break;
+}
+
+function operator() {
+    if ("+") {
+        return '+';
+        }
+    elseif ("-") {
+        return '-';
+        }
+    elseif ("*") {
+        return '*';
+        }
+    elseif ("/") {
+        return '/';
+        }
+    elseif ("%") {
+        return '%';
+        }
+}
+
+echo $input1, operator($input2), $input3;
+
