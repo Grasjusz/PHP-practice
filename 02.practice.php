03659cc9b9e9f8bedc828c462acb5ba2ec45e244
Grasjusz <46102483+grasjusz@users.noreply.github.com>
2024-12-22 15:20:44 +0100

    PHP practice

--- 02.practice.php
+++ 02.practice.php
@@ -1,18 +1,30 @@
 <?php
 
-echo"Starting for loop";
+echo"Starting for loop, red for even numbers, gold for odd \n";
 $int = 0;
-for ($int = 0; $int < 20; $int+=2){
-    echo "$int\n";
+for ($int = 0; $int < 20; $int+=1){
+    if ($int % 2 == 0){
+    echo "\033[31m $int \033[0m \n";
+    }
+    else{
+    echo "\033[33m $int \033[0m \n";
+    }
     }
 
-echo"Starting do while loop";
-$int2 = 0;
+echo"Starting do while loop, if digit /4 then double it\n";
+$int2 = 20;
 do {
-    echo "$int2\n";
-    $int2+=2;
+    if ($int2 % 4 ==0){
+        echo "$int2 - original integer\n";
+        $double = $int2 * $int2;
+        echo "\033[31m$double - doubled integer \033[0m \n";
+    }
+    else{
+    echo "\033[36m$int2 - cannot divide by 4!\033[0m \n";
+    }
+    $int2-=2;
 }
-while ($int2 < 20);
+while ($int2 > 0);
 
 $int3 = readline("Choose beetwen 0 - 10: ");
 $int4 = rand(0, 10);
